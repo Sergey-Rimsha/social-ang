59bb2bed421276a6c963a21ff5953b2b111008ad
Sergey Rimsha <Sergey.Rimsha.ru@gmail.com>
2023-06-28 21:58:55 +0300

    add is auth, and set isAuth true, and navigate if isAuth false to '/auth'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { AuthService } from './core/services/auth.service'
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'soc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'social-ang'
+  isAuth$: boolean
+  constructor(private authService: AuthService, private router: Router) {
+    this.isAuth$ = this.authService.isAuth
+  }
+
+  ngOnInit() {
+    if (!this.isAuth$) {
+      this.router.navigate(['/auth'])
+    }
+  }
 }
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,7 +1,14 @@
-import { Injectable } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
 import { Router } from '@angular/router'
 import { environment } from '../../../environments/environments'
+import { Injectable } from '@angular/core'
+
+interface ResponseAuthLogin<T> {
+  data: T
+  message: string[]
+  fieldsErrors: []
+  resultCode: number
+}
 
 @Injectable({
   providedIn: 'root',
@@ -12,8 +19,13 @@ export class AuthService {
   constructor(private http: HttpClient, private router: Router) {}
 
   login(data: Partial<{ email: string; password: string; rememberMe: boolean }>) {
-    this.http.post(`${environment.baseUrl}/auth/login`, data).subscribe(res => {
-      console.log(res)
-    })
+    this.http
+      .post<ResponseAuthLogin<{}>>(`${environment.baseUrl}/auth/login`, data)
+      .subscribe(res => {
+        if (res.resultCode === 0) {
+          this.isAuth = true
+          this.router.navigate(['/profile'])
+        }
+      })
   }
 }
